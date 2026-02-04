1bdff60d5b8cffcd6c0d164c185bc3bf52188d4a
lllllllrrrrrrr <lorenzo.restagno@gmail.com>
2026-02-04 23:15:34 +0800

    add test results and refactor test model classes for HCAN/no HCAN

--- tests/test.py
+++ tests/test.py
@@ -5,12 +5,13 @@ import torch.nn as nn
 import pandas as pd
 from torch import Tensor
 from torch.utils.data import Dataset
+from tqdm import trange
 
 WINDOW_SIZE = 256
 LOOKAHEAD = 21
 DTYPE = torch.float32
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-BATCH_SIZE = 1000
+BATCH_SIZE = 500
 
 
 class MyData(Dataset):
@@ -41,7 +42,7 @@ class MyData(Dataset):
         )
 
 
-class MyModel(nn.Module):
+class WithHCAN(nn.Module):
     def __init__(self):
         super().__init__()
         self.lstm = nn.LSTM(input_size=2, hidden_size=64, num_layers=3, batch_first=True)
@@ -59,25 +60,45 @@ class MyModel(nn.Module):
         return x
 
 
-def train_model():
+class NoHCAN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=2, hidden_size=64, num_layers=3, batch_first=True)
+        self.proj = nn.Sequential(
+            nn.Linear(64, 2),
+            nn.ReLU(),
+        )
+        return
+
+    def forward(self, x: Tensor) -> Tensor:
+        x, _ = self.lstm(x)
+        x = self.proj(x[:, -LOOKAHEAD:, :])
+        return x
+
+
+def train_model(model, model_name: str):
     data = MyData('train')
     dataloader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE)
-    model = MyModel().to(dtype=DTYPE, device=DEVICE)
+    model = model.to(dtype=DTYPE, device=DEVICE)
     model.train()
-    criterion = HCANLoss(y_trues=data.y_trues).to(device=DEVICE)
+    if model_name == 'NoHCAN':
+        criterion = nn.MSELoss().to(device=DEVICE)
+    else:
+        criterion = HCANLoss(y_trues=data.y_trues).to(device=DEVICE)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-    for epoch in range(50):
+    bar = trange(200, desc='Training ' + model_name, ncols=100)
+    for epoch in bar:
         epoch_loss = 0.0
         for inputs, targets in dataloader:
             optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, targets)['L_TOTAL']
+            loss = criterion(outputs, targets)
+            if model_name == 'WithHCAN':
+                loss = loss['L_TOTAL']
             loss.backward()
             optimizer.step()
             epoch_loss += float(loss.detach().cpu())
-
-        print(f'Epoch {epoch + 1}, Loss: {epoch_loss / BATCH_SIZE}')
+        bar.set_postfix({'loss': epoch_loss / BATCH_SIZE})
 
     # evaluation
     model.eval()
@@ -85,15 +106,21 @@ def train_model():
     all_y, all_x = [], []
     with torch.no_grad():
         for inputs, targets in dataloader:
-            outputs = model(inputs)['y_hat']
-            all_y.append(targets[..., 0].detach().cpu())
-            all_x.append(outputs[..., 0].detach().cpu())
+            outputs = model(inputs)
+            if model_name == 'WithHCAN':
+                outputs = outputs['y_hat']
+            all_y.append(targets.detach().cpu())
+            all_x.append(outputs.detach().cpu())
 
-    y = np.sign(torch.cat(all_y, dim=0).numpy())  # (N, T)
-    x = np.sign(torch.cat(all_x, dim=0).numpy())  # (N, T)
-    print('Accuracy:', np.mean(y == x))
+    y = torch.cat(all_y, dim=0).numpy()  # (N, T)
+    x = torch.cat(all_x, dim=0).numpy()  # (N, T)
+    print(model_name + ' sign accuracy:', np.mean(np.sign(y[..., 0]) == np.sign(x[..., 0])))
+    print(model_name + ' returns MSE:', np.mean((y[..., 0] - x[..., 0]) ** 2))
+    print(model_name + ' volatility MSE:', np.mean((y[..., 1] - x[..., 1]) ** 2))
+
     return
 
 
 if __name__ == '__main__':
-    train_model()
+    train_model(NoHCAN(), 'NoHCAN')
+    train_model(WithHCAN(), 'WithHCAN')
